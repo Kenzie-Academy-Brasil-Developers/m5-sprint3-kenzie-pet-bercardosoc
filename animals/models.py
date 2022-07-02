2e60f0bf7af8fe0e3e0b8d38d548d0950d1166e9
bercardosoc <bernardocardosodacosta@gmail.com>
2022-07-02 12:26:41 -0300

    refactor: recreate migrations

--- animals/models.py
+++ animals/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 class Animal(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    
+    group = models.ForeignKey(
+        to="groups.Group", on_delete=models.CASCADE, related_name="animals"
+    )
+    
+    characteristics = models.ManyToManyField(to="characteristics.Characteristic")
